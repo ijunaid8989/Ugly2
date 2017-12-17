5cca548451007cc8add1be4ee3940213b81094b4
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-17 21:16:54 +0500

    Added SQL execution.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def execute_statement(sql)
+    results = ActiveRecord::Base.connection.execute(sql)
+    if results.present?
+        return results
+    else
+        return nil
+    end
+  end
 end
