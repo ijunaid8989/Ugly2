ed75511edcf653f42a424820bdb10a74bcbf0bdb
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-22 22:08:49 +0500

    Added a TODO.

--- app/models/snapshot_extractor.rb
+++ app/models/snapshot_extractor.rb
@@ -17,6 +17,7 @@ class SnapshotExtractor < ApplicationRecord
     ApplicationController
       .helpers
       .execute_statement('SELECT s0."id", s0."from_date", s0."to_date", s0."interval", s0."schedule", f1."exid", f1."timezone", f1."name", s0."requestor" FROM "snapshot_extractors" AS s0 INNER JOIN LATERAL (SELECT * FROM cameras as cam WHERE cam.id = s0."camera_id") AS f1 ON TRUE WHERE (s0."status" = 1) ORDER BY s0."created_at" DESC LIMIT 1')
+      #Make the query in model ASSOC
   end
 
   def self.run_or_not(args)
