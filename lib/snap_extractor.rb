040d96306a4223f64f9a7299afa93bb2081afc04
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-25 17:54:03 +0500

    Added intervaling method.

--- lib/snap_extractor.rb
+++ lib/snap_extractor.rb
@@ -4,7 +4,23 @@ module SnapExtractor
     when []
       Rails.logger.info "No extrator with status 0"
     else
+      puts extractor
+      time_start = DateTime.now.utc
+      schedule = extractor.first["schedule"]
+      interval = intervaling(extractor.first["interval"])
+      requestor = extractor.first["requestor"]
+      camera_exid = extractor.first["camera_exid"]
+      binding.pry
       puts "I am a test from SnapExtractor Module #{extractor}"
+    end
+  end
+
+  def self.intervaling(interval)
+    case interval
+    when interval <= 0
+      1
+    else
+      interval
     end
   end
 end
