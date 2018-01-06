a728abed9e0457c1c3a252e6af4d117608f81c4f
Junaid Farooq <ijunaidfarooq@gmail.com>
2018-01-06 10:29:05 +0500

    Added date formation && Small self def.

--- lib/snap_extractor.rb
+++ lib/snap_extractor.rb
@@ -10,8 +10,64 @@ module SnapExtractor
       interval = intervaling(extractor.first["interval"])
       requestor = extractor.first["requestor"]
       camera_exid = extractor.first["camera_exid"]
+
+      timezone =
+        case extractor.first["timezone"]
+        when nil
+          "Etc/UTC"
+        else
+          extractor.first["timezone"]
+        end
+
+      start_date = ActiveSupport::TimeZone[timezone].parse(extractor.first["from_date"])
+      end_date = ActiveSupport::TimeZone[timezone].parse(extractor.first["to_date"])
+
+      total_days = round(((end_date - start_date) / 86400).round)
+
+      e_start_date = start_date.strftime("%A, %b %d %Y, %H:%M")
+      e_to_date = end_date.strftime("%A, %b %d %Y, %H:%M")
+      e_schedule = schedule
+      e_interval = humanize_interval(interval)
+
       binding.pry
-      puts "I am a test from SnapExtractor Module #{extractor}"
+    end
+  end
+
+  def.self humanize_interval(interval)
+    case interval
+    when 60
+      "1 Frame Every 1 min"
+    when 300
+      "1 Frame Every 5 min"
+    when 600
+      "1 Frame Every 10 min"
+    when 900
+      "1 Frame Every 15 min"
+    when 1200
+      "1 Frame Every 20 min"
+    when 1800
+      "1 Frame Every 30 min"
+    when 3600
+      "1 Frame Every hour"
+    when 7200
+      "1 Frame Every 2 hour"
+    when 21600
+      "1 Frame Every 6 hour"
+    when 43200
+      "1 Frame Every 12 hour"
+    when 86400
+      "1 Frame Every 24 hour"
+    when 1
+      "All"
+    end
+  end
+
+  def.self round(days)
+    case days
+    when 0
+      2
+    else
+      days + 1
     end
   end
 
